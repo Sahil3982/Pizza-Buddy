e61ebabd6922b1209a5b0db10a001d00f3b480c0
sahil3982 <113926133+Sahil3982@users.noreply.github.com>
2024-02-14 20:05:56 +0530

    .

--- Pizza Buddy/src/App.jsx
+++ Pizza Buddy/src/App.jsx
@@ -2,7 +2,7 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Cart from './Pages/Cart'
 import Pizzabuddy from './Pages/Pizzabuddy'
-import {BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Mynavbar from './Components/Header/Mynavbar'
 import Products from './Pages/Products'
 import './App.css';
@@ -11,17 +11,17 @@ function App() {
   return (
     <>
 
-    <Router>
-      <Mynavbar />
+      <Router>
+        <Mynavbar />
 
-       <Routes>
-       <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/pizzabuddy' element={<Pizzabuddy />} />
-       </Routes>
-    </Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/pizzabuddy' element={<Pizzabuddy />} />
+        </Routes>
+      </Router>
     </>
   )
 }
