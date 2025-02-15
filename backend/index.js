efd6e5e4a65e388435d4b151ebde7b5a959d7b8e
Sahil3982 <sahilpatel3982@gmail.com>
2025-02-15 17:21:45 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,29 @@
 import express from 'express';
-// Import database connection and routers
 import db from './src/config/db.js';
-import cors from 'cors'
+import cors from 'cors';
 import userRouter from './src/user/userRouter.js';
-import recipelist from './src/user/recpielist.js';
+import recipelist from './src/user/recipelist.js';
 import addrecipe from './src/user/addrecipe.js';
 
-// Initialize express app
 const app = express();
 
-// Middleware for JSON parsing
+// Middleware
 app.use(express.json());
-
-// Connect to the database
-
-// OR if you want to restrict access to your frontend only:
 app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"] }));
 
- 
+// Connect to the database
 db();
 
 // Setup routes
 app.use('/api/user', userRouter);
-app.use('/api', recipelist);
-app.use('/api',addrecipe)
+app.use('/api/recipes', recipelist); 
+app.use('/api/recipes', addrecipe);
+
+app.get('/list', (req, res) => res.send("list"));
+app.get('/list/:id', (req, res) => res.send(`list id: ${req.params.id}`));
 
 // Initialize the server
-const PORT = 3000;
+const PORT = 3002;
 app.listen(PORT, () => {
     console.log(`Server is running and listening on port: ${PORT}`);
 });
